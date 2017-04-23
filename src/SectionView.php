4a67164ad345bfc5ff9260fa3b42f43c7bb0d289
kamrava <h.kamrava@yahoo.com>
2017-04-23 23:10:46 +0430

    Minor Refactoring

--- src/SectionView.php
+++ src/SectionView.php
@@ -20,14 +20,14 @@ class SectionView {
     public function render()
     {
         $sections = [
-            'title' => view($this->path.'.partials._title')->with($this->data)->render(),
-            'head'  => view($this->path.'.partials._head' )->with($this->data)->render(),
-            'body'  => view($this->path.'.partials._body' )->with($this->data)->render(),
-            'foot'  => view($this->path.'.partials._foot' )->with($this->data)->render()
+            'title' => view($this->path.'.sections._title')->with($this->data)->render(),
+            'head'  => view($this->path.'.sections._head' )->with($this->data)->render(),
+            'body'  => view($this->path.'.sections._body' )->with($this->data)->render(),
+            'foot'  => view($this->path.'.sections._foot' )->with($this->data)->render()
         ];
         $output = $this->addSlashesAndRemoveLines($sections);
-        $partials = json_decode(json_encode($output, JSON_FORCE_OBJECT));
-        return view($this->path.'.spf_json', ['partials' => $partials]);
+        $sections = json_decode(json_encode($output, JSON_FORCE_OBJECT));
+        return view($this->path.'.spf_json', ['sections' => $sections]);
     }
 
     private function addSlashesAndRemoveLines($sections)
